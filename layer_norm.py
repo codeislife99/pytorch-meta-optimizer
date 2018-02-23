68d4cfd5d7fd2e55cc0ccebb8b0bc44dbb9a5e9b
Ritwik Das <ritwikdas54@gmail.com>
2018-02-23 14:33:02 -0500

    Added comments

--- layer_norm.py
+++ layer_norm.py
@@ -13,7 +13,12 @@ class LayerNorm1D(nn.Module):
         self.bias = nn.Parameter(torch.zeros(1, num_outputs))
 
     def forward(self, inputs):
+        # Taking the mean/std of columns.
+        # keepdim = true will preserve the dimensionality of the input else input will be squeezed
+        # expand_as will preserve the same size as input and repeat the mean/std along the columns(1st dimension)  
         input_mean = inputs.mean(1,keepdim=True).expand_as(inputs)
         input_std = inputs.std(1,keepdim=True).expand_as(inputs)
-        x = (inputs - input_mean) / (input_std + self.eps)
+        x = (inputs - input_mean) / (input_std + self.eps) 
+        # No of columns in x is equal to the num_outputs. 
+        # This expand_as tries to expand the 1st row of self.weight/self.bias into the no of rows of x. 
         return x * self.weight.expand_as(x) + self.bias.expand_as(x)
